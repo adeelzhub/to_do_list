84e065a28525f05314e00b94e9889d9d9ef59d13
adeelzhub <adeelijaz@hotmail.com>
2021-08-10 14:53:44 +0100

    Added alert

--- app.js
+++ app.js
@@ -14,19 +14,22 @@ input.addEventListener("click",()=>{
     
 
 buttonAdd.addEventListener("click",()=>{
-    if(input.value != "" ){
+    if(input.value == "" || input.value === "Your Text" ){
+        alert("Please enter a text in the box.")
+    }else{
         list.innerHTML+= `<li>${input.value}</li>`
         input.value = "Your Text";
         input.style.color = "grey";
         heading4.innerText = `There are ${document.querySelectorAll("li").length} things on  your 'To Do List' today.`;
-    }else{
-        alert("Please enter a text in the box.")
     }})
     
     
 
 buttonClear.addEventListener("click",()=>{
-    list.innerHTML = ""
-    input.style.color = "grey"
-    heading4.innerText = "To Do List is empty"
-})
+    if(list.innerHTML == ""){
+        alert("The list is empty.")
+    }else{
+        list.innerHTML = ""
+        input.style.color = "grey"
+        heading4.innerText = "To Do List is empty"
+}})
